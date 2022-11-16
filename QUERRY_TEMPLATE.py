35ff058a375ac4b26689ccd9deffe1c961b28d58
QuentinWESSER <quentin.wesser@edu.esiee.fr>
2022-11-16 13:32:59 +0100

    Add new QUERRY

--- QUERRY_TEMPLATE.py
+++ QUERRY_TEMPLATE.py
@@ -21,3 +21,35 @@ query GetVideoGame($IdRange: [ID]) {
 VIDEOGAME_QUERRY_VAR = {
   "IdRange": []
 }
+
+TOURNAMENTS_QUERRY = """
+query GetVideoGame($IdRange: [ID], $Loca: String, $Range: String, $Start: Timestamp, $End: Timestamp) {
+  tournaments(query: {
+    perPage : 500
+    filter: {
+      videogameIds: $IdRange,
+      location: {
+        distanceFrom: $Loca,
+        distance: $Range
+      },
+      afterDate: $Start,
+      beforeDate: $End
+    }
+  }){
+    nodes {
+      id
+    },
+    pageInfo {
+      totalPages
+    }
+  }
+}
+"""
+
+TOURNAMENTS_QUERRY_VAR = {
+  "IdRange" : [], 
+  "Loca": "",
+  "Range": "" ,
+  "Start": 0, 
+  "End": 0
+}
